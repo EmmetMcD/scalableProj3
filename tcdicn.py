741c6058a0bd59a32d0c4227f4a0be01a74bc7ea
EmmetMcD <33671085+EmmetMcD@users.noreply.github.com>
2023-11-15 21:35:32 +0000

    fixing encoding

--- tcdicn.py
+++ tcdicn.py
@@ -512,7 +512,7 @@ class Client:
 def encrypt(data: str,key: bytes):
         data = str(data)
         fernet = Fernet(key)
-        return fernet.encrypt(data.encode())
+        return fernet.encrypt(data).encode()
         # data = str(data)
         # bData = int.from_bytes(bytes(data, "utf-8"),"big")
         # bKey = int.from_bytes(bytes(key,"utf-8"),"big")
